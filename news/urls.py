dd4b97d578f3edb2873e38ba59fddee062a936aa
Volodymyr Shpyhun <vladimir.shpigun@gmail.com>
2022-08-14 21:49:22 +0300

    add  /

--- news/urls.py
+++ news/urls.py
@@ -6,19 +6,19 @@ from .views import NewsView, AddNewsView, NewsDetail, CategoryView, LikeView, Cr
     CategoryAPIView, ProfileUpdate, NewsViewSet, NewsDetailAPIView
 
 router = routers.SimpleRouter()
-router.register('api/v1/comments', CreateCommentViewSet, basename='comments')
-router.register('api/v1/news', NewsViewSet, basename='news')
-router.register('api/v1/category', CategoryAPIView, basename='category')
+router.register('api/v1/comments/', CreateCommentViewSet, basename='comments')
+router.register('api/v1/news/', NewsViewSet, basename='news')
+router.register('api/v1/category/', CategoryAPIView, basename='category')
 
 
 urlpatterns = [
-    # path('', cache_page(60)(NewsView.as_view()), name='news_list'),
+
     path('', NewsView.as_view(), name='news_list'),
     path('news/add_news/', AddNewsView.as_view(), name='add_news'),
     path('news/<slug:news_slug>/', NewsDetail.as_view(), name='news_detail'),
     path('category/<slug:category_slug>/', CategoryView.as_view(), name='category'),
-    path('like/<int:pk>', LikeView.as_view(), name='like_news'),
+    path('like/<int:pk>/', LikeView.as_view(), name='like_news'),
     path('api/v1/auth/', include('rest_framework.urls')),
     path('api/v1/profile/', ProfileUpdate.as_view(), name='profile'),
-    path('api/v1/news/<int:pk>', NewsDetailAPIView.as_view(), name='detail-list'),
+    path('api/v1/news/<int:pk>/', NewsDetailAPIView.as_view(), name='detail-list'),
 ]
